7dd7a71553d2bee97957839e569b4c5d9630c6c6
Анна Афанасьева <ann.afanaseva777@gmail.com>
2026-03-23 00:14:42 +0300

    new case

--- app/components/case-list-block/case-list.ts
+++ app/components/case-list-block/case-list.ts
@@ -1,9 +1,15 @@
 import caseImageLeadGeneration from "/case-list/lead-generation.png";
+import avitoAds from "/case-list/avito-ads.png";
 
 export const servicesList = [
    {
       label: "Интервью с экспертом: что такое кадровая лидогенерация?",
       image: caseImageLeadGeneration,
       href: "/case-lead-generation",
+   },
+   {
+      label: "Avito Pro и Avito Ads в массовом рекрутменте: в чем разница и что эффективнее?",
+      image: avitoAds,
+      href: "/case-avito-ads",
    },
 ];
